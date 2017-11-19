00deaf6ba7a99a7e294273a7c21cded3e0194eb4
DESKTOP-AMP2N8L\youp <youpkuiper@gmail.com>
2017-11-19 16:38:43 +0100

    hallo

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2,7 +2,7 @@ const settingsService = require('../services/settings-service');
 
 module.exports = function (app, db) {
     app.put('/Settings', (req, res) =>{
-        settingsService.putSetting(db, setting, (error, result) => {
+        settingsService.putSetting(db, req.body, req.query.userId, (error, result) => {
             if(error){
                 res.send(error);
             }else{
