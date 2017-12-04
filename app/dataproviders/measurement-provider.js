aa3abf62df083ec6cb1eb017ba2726fcc4b38553
youp <youpkuiper@gmail.com>
2017-12-04 14:47:30 +0100

    postmeasurement update

--- app/dataproviders/measurement-provider.js
+++ app/dataproviders/measurement-provider.js
@@ -1,16 +1,13 @@
 const ObjectId = require('mongodb').ObjectID;
 
 module.exports = {
-    postMeasurement: function (db, measurement, callback) {
+    postMeasurement: function (db, measurement, userId, callback) {
         let date = new Date();
-        date.setDate(date.getDate()-20)
-        measurement = null;
-        let userId = "5a0ad4ac9b582a11641e12fa";
+        date.setDate(date.getDate());
         userId = ObjectId(userId);
-        measurement = {
-            userId: userId,
-            measurementDateTime: date
-        };
+        measurement.userId = userId;
+        measurement.measurementDateTime = date;
+
         db.collection('measurements').insertOne(measurement, (error, result)=> {
             if(error){
                 callback(error);
--- app/routes/measurements.js
+++ app/routes/measurements.js
@@ -3,7 +3,7 @@ const measurementService = require('../services/measurement-service');
 module.exports = function (app, db) {
 
     app.post('/Measurements', (req, res) => {
-        measurementService.postMeasurement(db, req.body, (error, result) => {
+        measurementService.postMeasurement(db, req.body, req.query.userId,  (error, result) => {
             if(error){
                 res.send(error);
             }else{
