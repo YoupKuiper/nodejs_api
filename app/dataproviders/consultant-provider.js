9e7709a3c5db7aa8ebce8c7d7b4bd296bfa25e45
Youp Kuiper <youpkuiper@gmail.com>
2018-10-03 19:01:18 +0200

    Initial project setup

--- app/dataproviders/consultant-provider.js
+++ app/dataproviders/consultant-provider.js
@@ -1,10 +1,11 @@
 module.exports = {
 
     getAllConsultants: function (db, callback) {
+        console.log(db)
         db.collection('consultants').find().sort({"firstname": 1}).toArray((error, consultants) => {
-            if(error){
+            if (error) {
                 callback(error);
-            }else{
+            } else {
                 callback(null, consultants);
             }
         })
